936623f75e7d6d4454a62f24130686a2da491826
alejo9424 <alejandro.castaneda@payco.co>
2021-01-27 17:23:29 -0500

    fix(Corrección cashModel): Se realiza corrección en el cashmodel para q reciba parametros como string

--- EpaycoSdk/Models/Cash/CashModel.cs
+++ EpaycoSdk/Models/Cash/CashModel.cs
@@ -16,9 +16,9 @@ namespace EpaycoSdk.Models.Cash
         public int ref_payco { get; set; }
         public string factura { get; set; }
         public string descripcion { get; set; }
-        public int valor { get; set; }
-        public int iva { get; set; }
-        public int baseiva { get; set; }
+        public string valor { get; set; }
+        public string iva { get; set; }
+        public string baseiva { get; set; }
         public string moneda { get; set; }
         public string banco { get; set; }
         public string estado { get; set; }

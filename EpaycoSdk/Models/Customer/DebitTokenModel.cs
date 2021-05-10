10c45c89fc8b1f3cf51aa392586468d1a4beb4e8
roberto-epayco <romedu1@hotmail.com>
2021-05-10 16:16:20 -0400

    feat: Create the debit token model

---
+++ EpaycoSdk/Models/Customer/DebitTokenModel.cs
@@ -0,0 +1,28 @@
+namespace EpaycoSdk.Models
+{
+    public class DebitTokenModel
+    {
+        public bool status { get; set; }
+        public string id { get; set; }
+        public bool success { get; set; }
+        public string type { get; set; }
+        public Data data { get; set; }
+        public DebitAccount debitAccount { get; set; }
+    }
+
+    public partial class Data
+    {
+        public string status { get; set; }
+        public string id { get; set; }
+        public string created { get; set; }
+        public bool livemode { get; set; }
+        public string description { get; set; }
+        public string errors { get; set; }
+        
+    }
+
+    public class DebitAccount
+    {
+        public string mask { get; set; }
+    }
+}
